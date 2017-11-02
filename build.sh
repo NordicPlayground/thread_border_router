7a5e4583a16826735c5e73f18d1a8ac2ed88d9ab
Piotr Szkotak <piotr.szkotak@nordicsemi.no>
2017-11-02 15:17:49 +0100

    Add Eclipse Distribution License for MQTT-SN Gateway

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd openwrt
 make V=s -j4
 find ./bin -name "openwrt-*sdcard.img" -exec cp {} ../RaspPi_Thread_Border_Router_Demo.img \;
 cd ..
-zip RaspPi_Thread_Border_Router_Demo.zip RaspPi_Thread_Border_Router_Demo.img NXP_EXPLORE-NFC-WW_LICENSE.pdf OpenWRT_LICENSE.txt RaspberryPi_Border_Router_LICENSE.txt
+zip RaspPi_Thread_Border_Router_Demo.zip RaspPi_Thread_Border_Router_Demo.img NXP_EXPLORE-NFC-WW_LICENSE.pdf OpenWRT_LICENSE.txt RaspberryPi_Border_Router_LICENSE.txt Paho_MQTT-SN_Eclipse_Distribution_LICENSE.txt
 #in case the make fails tarballs need to be downloaded manually to the 'dl/' directory, e.g.:
 #wget http://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v1.43.1/e2fsprogs-1.43.1.tar.gz -P dl/
 
